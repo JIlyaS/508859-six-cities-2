8678fc221a193b221a2dcde83d9143f94761f8f4
JIlyaS <ilkolmakov@yandex.ru>
2019-10-25 11:37:21 +0500

    add mock data

---
+++ src/mocks/offers.js
@@ -0,0 +1,34 @@
+export const offers = [
+  {
+    img: `apartment-01.jpg`,
+    isPremium: true,
+    cost: 120,
+    title: `Beautiful luxurious apartment at great location`,
+    type: `Apartment`,
+    rating: 93
+  },
+  {
+    img: `room.jpg`,
+    isPremium: false,
+    cost: 120,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    rating: 80
+  },
+  {
+    img: `apartment-02.jpg`,
+    isPremium: false,
+    cost: 120,
+    title: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    rating: 80
+  },
+  {
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    cost: 180,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    rating: 100
+  }
+];
