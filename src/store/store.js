529147279b98024f516cefbfa0b7943919d0ef29
Ilya Kolmakov <ilkolmakov@yandex.ru>
2019-11-18 13:58:08 +0500

    fix name store reducer

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,13 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import compose from 'recompose/compose';
 
-import reducers from '../reducer/reducer';
+import reducer from '../reducer/reducer';
 import {configureAPI} from '../api';
 
 const api = configureAPI((...args) => store.dispatch(...args));
 
 const store = createStore(
-    reducers,
+    reducer,
     compose(
         applyMiddleware(thunk.withExtraArgument(api)),
         composeWithDevTools()
